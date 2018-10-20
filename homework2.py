abecae55ea855129be8e15d6965c1ae54cb30357
Zachary Nelson <zacharynelson@Zacharys-MacBook-Air.local>
2018-10-19 21:16:47 -0600

    Adding Homework 2.

---
+++ homework2.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+def callPayoff(spot, strike):
+    return np.maximum(spot - strike, 0.0)
+
+def putPayoff (spot, strike):
+    return np.maximum(strike - spot, 0.0)
+
+r = .08
+v = .3
+q = 0.0
+expiry = 1.0
+n = 1.0
+h = eqpiry / n
+S = 100
+K = 95
+
+u = np.exp{(r-q) * h + v * np.sqrt(h)}
+d = np.exp{(r-q) * h - v * np.sqrt(h)}
